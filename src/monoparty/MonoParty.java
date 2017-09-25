79ebe50afa5ca8c11476a6fcb7a06fbb5ba3af19
Michael Iula <michaeliula2@gmail.com>
2017-09-25 11:59:44 -0400

    Another test

--- src/monoparty/MonoParty.java
+++ src/monoparty/MonoParty.java
@@ -16,7 +16,7 @@ public class MonoParty {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        //mike
+        //I'm got access to this shit!!!!
     }
     
 }
