c0978e14202303d70a5d906a0e1edbb726f3810c
DanielGauthier8 <dangauthier8@gmail.com>
2017-10-23 11:54:01 -0400

    Update Player.java

--- src/monoparty/Player.java
+++ src/monoparty/Player.java
@@ -1,88 +1,96 @@
-import java.awt.Color;
-import java.awt.Graphics;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package monoparty;
 
 public class Player {
-	private int points;
-	private int xPos;
-	private int yPos;
-	private int size;
-	private Color pColor;
-	private String pName;
-	
-	public Player (){
-		points = 0;
-		xPos = 25;
-		yPos = 25;
-		pColor = Color.red;
-		size = 50;
-		pName = "New Player";
-	}
-	
-	public Player (int point, int x, int y, Color color, String name){
-		points = point;
-		xPos = x;
-		yPos = y;
-		pColor = color;
-		size = 50;
-		pName = name;
-	}
-	
-	public String getName(){
-		return pName;
-	}
-	
-	public void setName(String neoName){
-		pName = neoName;
-	}
-	
-	public int getPoints(){
-		return points;
-	}
-	
-	public void setPoints(int p){
-		points = p;
-	}
-	
-	public int getXPos (){
-		return xPos;
-	}
-	
-	public void setXPos (int xPosition){
-		xPos = xPosition;
-	}
-	
-	public int getYPos (){
-		return yPos;
-	}
-	
-	public void setYPos (int yPosition){
-		yPos = yPosition;
-	}
-	
-	public Color getColor (){
-		return pColor;
-	}
-	
-	public void setColor (Color neoColor){
-		pColor = neoColor;
-	}
-	
-	public int getSize(){
-		return size;
-	}
-	
-	public void setSize(int neoSize){
-		size = neoSize;
-	}
-	
-	public void move (){
-		
-		
-		
-	}
-	
-	public void paint(Graphics pane){
-		pane.setColor(pColor);
-		pane.fillOval(xPos, yPos, size, size);
-	}
+    //Data fields...............
+    //name (String)
+    // Player name
+    // Default: "Player One"    
+    private String name;
+    //score (int)
+    // Default: 0
+    // min: 0; max: No Limit
+    private int score = 0;
+    //score (int)
+    // Default: 0
+    // min: 0; max: Number of spaces
+    private int place = 0;
+    
+    //Constuctors...............
+    /** 
+     * Default player initialization
+     */
+    public Player()
+    {
+        this("Player One");
+        score = 0;
+    }
+    
+    /** 
+     * Specific student initialization and 
+     * assigns name and a pre-defined textbooks to the student
+     * @param myName name of student
+     */
+    
+    public Player(String myName)
+    {
+        name = myName;
+        score = 0;
+    }
+    
+    //Methods.......................
+    // Accessor methods
+     /**
+     * Accessor method for player name
+     * @return name of player
+     */
+    public String getName()
+    {
+        return name;
+    }
+    
+    
+    /**
+     * Accessor method for player's current health status
+     * @return player's health data field
+     */    
+    public double getScore()
+    {
+        return score;
+    }
+    
+    
+    // Mutator Methods  
+    /**
+     * Setter for player name, no validation on name
+     * @param myName the new name of player
+     */    
+    public void setName(String myName)
+    {
+        name = myName;
+    }
+    
+    //The player's score
+    public double changeScoreBy(int thePoints)
+    {
+        //Damage has to be negative.  Prevent cheating by the creature using abs
+        score += thePoints;
+        //Don't let health go negative
+        return score;
+    }
+    //The player's score
+    
+    //Add to the postion of the student
+    public double playerPostion(int placesEarned)
+    {
+        //Damage has to be negative.  Prevent cheating by the creature using abs
+        place += placesEarned;
+        //Place will be max of 36
+        place = place % 36;
+        return place;
+    }
 }
