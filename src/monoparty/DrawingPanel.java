5a3476e322dda0906428e24a97a1dcf381d6913f
DanielGauthier8 <dangauthier8@gmail.com>
2017-10-24 14:31:01 -0400

    Update DrawingPanel.java

--- src/monoparty/DrawingPanel.java
+++ src/monoparty/DrawingPanel.java
@@ -1,9 +1,15 @@
 
 
 import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
 import javax.swing.JPanel;
 import java.awt.Graphics;
 import java.awt.Point;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+import javax.swing.JLabel;
 
 /**
  *
@@ -17,28 +23,42 @@ public class DrawingPanel extends JPanel{
     private DrawablePlayer theBot;
     //The location
     private int thePlace;
+    //Font variable
+    private final Font myFont;
+    //Button to throw the heaviest button
+    private final JButton addOneToSpace;
     DrawingPanel()
     {
     //The player
         thePlayer = new DrawablePlayer(100,100, Color.GREEN);
         //The computer player
         theBot = new DrawablePlayer(200,100,Color.RED);
+        myFont = new Font("Times New Roman",Font.PLAIN,28);
+        //Buttons
+        //Testing adding to player location
+        addOneToSpace = new JButton("Add");
     }
     
     @Override
     public void paintComponent(Graphics pen)
     {
+        
+        //Paint over because Windows
+        super.paintComponent(pen);
         //Draw the player
         thePlayer.draw(pen);
         //The bot
-        theBot.draw(pen);
-        
-        
-        
-        
+        //theBot.draw(pen);
+        pen.setColor(Color.BLACK);
+        pen.setFont(myFont);
+        pen.drawString("MonoParty", 360, 450);
         //After game play update player location
-        thePlayer.moveTo(playerLocaton(thePlayer));
-        theBot.moveTo(playerLocaton(theBot));
+        //theBot.moveTo(playerLocaton(theBot));
+    }
+    
+    public void playGame() throws InterruptedException
+    {
+        theButtons();
     }
     
     private Point playerLocaton(DrawablePlayer thatPlayer)
@@ -47,12 +67,12 @@ public class DrawingPanel extends JPanel{
         thePlace = thatPlayer.getPlace();
         if(thePlace >= 10)
         {
-            thePoint.x = thePlace + 10;
+            thePoint.x = (thePlace*100) + 100;
             thePoint.y = 100; 
         }
         else if(thePlace >= 20)
         {
-            thePoint.x = thePlace;
+            thePoint.x = thePlace; 
             thePoint.y = 1000;  
         }
         if(thePlace >= 30)
@@ -66,5 +86,35 @@ public class DrawingPanel extends JPanel{
             thePoint.y = thePlace; 
         }
         return thePoint;
+    }
+    //Listen for clicks
+    private class ButtonListener implements ActionListener
+        {
+            //When a button is clicked the command is put in this method
+            @Override
+            public void actionPerformed(ActionEvent buttonClick) 
+            {
+                //Put the command in a String
+                String command = buttonClick.getActionCommand();
+                //See if the button 1 was clicked and has books in it
+                if(command.equals("Add"))
+                {
+                    thePlayer.changeScoreBy(1);
+                    thePlayer.moveTo(playerLocaton(thePlayer));
+                }
+                //Draw the book according to what is being clicked on
+                repaint();
+            }
+        }
+    private void theButtons()
+    {
+        //Use the button listener object
+        ButtonListener listener = new ButtonListener();
+        //Give the value of the button to be sent to method
+        addOneToSpace.setActionCommand("Add");
+        //Listen for the click
+        addOneToSpace.addActionListener(listener);
+        //Draw the button
+        add(addOneToSpace);
     }
 }
