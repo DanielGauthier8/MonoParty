35ae3143d2919cdabc2ec0a01527ab9732d6139d
DanielGauthier8 <dangauthier8@gmail.com>
2017-11-16 00:14:04 -0500

    Update DrawableGame.java

--- src/monoparty/DrawableGame.java
+++ src/monoparty/DrawableGame.java
@@ -12,8 +12,36 @@ public class DrawableGame extends Settings implements DrawableInterface{
 	private Color boardColor;
 	private int x, y;
 	private BoardSpaces [] bSpaces;
-	
+	//Font variable
+        private final Font headings;
+        //Font variable
+        private final Font text;
+        //The main game color
+        private final Color primaryColor;
+        //Secondary color
+        private final Color secondaryColor;
+        //Accent or tertiary color
+        private final Color accentColor;
+        //The text color
+        private final Color textColor;
+        //White point
+        private final Color whitePoint;
+        //Start x
+        private int theX;
+        //Start y
+        private int theY;
+    
 	public DrawableGame(){
+            primaryColor = new Color(85,140,137);
+            textColor = new Color(64,64,64);
+            secondaryColor = new Color(116,175,173);
+            accentColor = new Color(217,133,59);
+            whitePoint = new Color(236,236,234);
+
+            theX = 0;
+            theY = 0;
+            headings = new Font("Times New Roman",Font.PLAIN,28);
+            text = new Font("Times New Roman",Font.PLAIN,16);
 		boardHeight = 1280;
 		boardWidth = 720;
 		boardColor = Color.blue;
@@ -45,37 +73,57 @@ public class DrawableGame extends Settings implements DrawableInterface{
 		boardColor = color;
 	}
 	
-	public BoardSpaces[] generateSpaces(){
+        public BoardSpaces[] generateSpaces(){
 		x = -128;
 		y = -72;
 		int i = 0;
+		int j = 0;
 		
-		while (x < boardWidth - 128){
-			x = x + 128;
-			bSpaces[i] = new BoardSpaces(x, 0, Color.orange, i);
+		while (i < bSpaces.length/4){
+			x = x + 127;
+			bSpaces[j] = new BoardSpaces(x + 0, 0, j);
 			i++;
+			j ++;
 		}
 		
-		while (y > boardHeight -72){
-			bSpaces[i] = new BoardSpaces(boardWidth - 128, y + 72, Color.orange, i);
+		i = 0;
+		
+		while (i < bSpaces.length/4){
+			y = y + 72;
+			bSpaces[j] = new BoardSpaces(1140, y, j);
 			i++;
+			j ++;
 		}
-		while (x > boardHeight -128){
-			bSpaces[i] = new BoardSpaces(x - 128, y + 72, Color.orange, i);
+		
+		i = 0;
+		x = 1280;
+		
+		while (i < bSpaces.length/4){
+			x = x - 127;
+			bSpaces[j] = new BoardSpaces(x - 13 , 648, j);
 			i++;
+			j ++;
 		}
-		while (y < boardWidth -72){
-			bSpaces[i] = new BoardSpaces(0, y - 72, Color.orange, i);
+		
+		i = 0;
+		y = 720;
+
+		while (i < bSpaces.length/4){
+			y = y - 72;
+			bSpaces[j] = new BoardSpaces(0, y, j);
 			i++;
+			j ++;
 		}
+		
 		
 		return bSpaces;
 		
 	}
 	
 	public void paint(Graphics pane){
-		
-		
+		for (int j = 0; j < bSpaces.length; j++){
+			bSpaces[j].paint(pane);
+		}
 	}
 
     @Override
@@ -130,7 +178,14 @@ public class DrawableGame extends Settings implements DrawableInterface{
 
     @Override
     public void draw(Graphics pen) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+                
+        //Background
+        pen.setColor(primaryColor);
+            pen.fillRect(theX + 0, 0, 1280, 720);
+      
+        for (int j = 0; j < bSpaces.length; j++){
+			bSpaces[j].paint(pen);
+		}
     }
 	
 }
