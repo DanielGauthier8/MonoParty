1de2dfa1770072b7f22f08c45bdca3eb3d514106
DanielGauthier8 <dangauthier8@gmail.com>
2017-12-05 15:18:08 -0500

    Update Player.java

--- src/monoparty/Player.java
+++ src/monoparty/Player.java
@@ -1,104 +1,143 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+package dodger;
+
+import java.awt.event.*;
+import javax.swing.ImageIcon;
+import java.awt.Color;
+import java.awt.Graphics;
+
+import javax.swing.JPanel;
+import javax.swing.Timer;
+
+public class Player extends JPanel {
+	//Timer t = new Timer(5,this);
+    private boolean invulnerable = false;
+    private int lives = 3;
+    private final int size=25;
+    private int x=640, y=600;
+    double velx =0, vely=0;
+    int xL = 0,xR = 1280,yU =0 ,yD = 690;
+    private int vkUp, vkDown, vkLeft, vkRight;
+    Color color = new Color(255, 0, 0);
 
 
-public class Player {
-    //Data fields...............
-    //name (String)
-    // Player name
-    // Default: "Player One"    
-    private String name;
-    //score (int)
-    // Default: 0
-    // min: 0; max: No Limit
-    private int score = 0;
-    //score (int)
-    // Default: 0
-    // min: 0; max: Number of spaces
-    private int place = 0;
-    
-    //Constuctors...............
-    /** 
-     * Default player initialization
-     */
-    public Player()
-    {
-        this("Player One");
-        score = 0;
+    public Player(int vkUp, int vkDown, int vkLeft, int vkRight) {
+            // TODO Auto-generated constructor stub
+             this.vkUp = vkUp;
+         this.vkDown = vkDown;
+         this.vkLeft = vkLeft;
+         this.vkRight = vkRight;
+            setFocusable(true);
+            setFocusTraversalKeysEnabled(false);
+
+    }
+
+    //public void player(int up, int down,int left,int right){
+            //t.start();
+            //addKeyListener(this);
+
+    //}
+
+    public boolean getInvulnerable() {
+        return invulnerable;
     }
     
-    /** 
-     * Specific student initialization and 
-     * assigns name and a pre-defined textbooks to the student
-     * @param myName name of student
-     */
-    
-    public Player(String myName)
-    {
-        name = myName;
-        score = 0;
+    public void setInvulnerable(boolean bool) {
+        invulnerable = bool;
     }
     
-    //Methods.......................
-    // Accessor methods
-     /**
-     * Accessor method for player name
-     * @return name of player
-     */
-    public String getName()
-    {
-        return name;
+    @Override
+    public int getWidth() {
+        return this.size;
+    }
+
+    @Override
+    public int getHeight() {
+        return this.size;
+    }
+
+    @Override
+    public int getX() {
+        return this.x;
+    }
+
+    @Override
+    public int getY() {
+        return this.y;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
+    }
+
+    public void subtractLife() {
+        this.lives--;
     }
     
-    
-    /**
-     * Accessor method for player's current health status
-     * @return player's health data field
-     */    
-    public double getScore()
-    {
-        return score;
+    public int getLives() {
+        return this.lives;
     }
-    
-    /**
-     * Accessor method for player's current health status
-     * @return player's health data field
-     */    
-    public int getPlace()
-    {
-        return place;
+
+    public void update(){
+        if(x>xL){
+            if(velx<0) {
+            x += velx;
+            }
+
+        }
+        if(x<xR-30) {
+            if(velx>0) { 
+                x += velx;
+            }
+        }
+        if(y>yU) {
+            if(vely<0) {
+                y += vely;
+            }
+        }
+        if(y<yD-30) {
+            if(vely>0) {
+                y += vely;
+            }
+        }
+            /*if(x>xL && x<xR-30 && y>yU && y<yD-30){
+            //repaint();
+            x += velx;
+            y += vely;
+            }*/
+
+
     }
-    
-    // Mutator Methods  
-    /**
-     * Setter for player name, no validation on name
-     * @param myName the new name of player
-     */    
-    public void setName(String myName)
-    {
-        name = myName;
+    public void pressed(int keyCode) {
+        if (keyCode == vkUp){
+        	vely = -2.5;
+        	velx = 0;
+        }
+        else if (keyCode == vkDown){
+        	vely = 2.5;
+        	velx = 0;
+        }
+        else if (keyCode == vkLeft){
+           	velx = -2.5;
+           	vely = 0;
+        }
+        else if (keyCode == vkRight){
+            velx = 2.5;
+            vely = 0;
+        }
     }
-    
-    //The player's score
-    public double changeScoreBy(int thePoints)
-    {
-        //Damage has to be negative.  Prevent cheating by the creature using abs
-        score += thePoints;
-        //Don't let health go negative
-        return score;
+
+    public void released(int keyCode) {
+        if (keyCode == vkUp || keyCode == vkDown)
+            vely = 0;
+        else if (keyCode == vkLeft || keyCode == vkRight)
+        	velx = 0;
     }
-    //The player's score
-    
-    //Add to the postion of the student
-    public double playerPostion(int placesEarned)
-    {
-        //Damage has to be negative.  Prevent cheating by the creature using abs
-        place += placesEarned;
-        //Place will be max of 36
-        place = place % 36;
-        return place;
-    }
+
+
+	public void paint(Graphics g){
+            g.setColor(color);
+	    //update();
+	    g.fillRect(x,y,size,size);
+	    // t.start();
+		}
 }
